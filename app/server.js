a7166c9408a491cf9a93f2065dda2e28e6c84615
Joaquin Villanueva <74026054+JoaquinVF@users.noreply.github.com>
2022-07-13 17:40:03 -0300

    add Post method for email send

--- app/server.js
+++ app/server.js
@@ -3,43 +3,54 @@ var express = require('express'),
     nodeMailer = require('nodemailer'),
     bodyParser = require('body-parser');
 
-    var emailHost = "saga.mensajes@gmail.com";
+    var emailUser = "saga.mensajes@gmail.com";
+    var emailPassword = "uliqlcqkoaibeqrh";
 
     var app = express();
-    app.set('view engine', 'ejs');
     app.use(express.static('public'));
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
-    var port = 3000;
-    app.get('/', function (req, res) {
-      res.render('index');
-    });
-    app.post('/send-email', function (req, res) {
+    var port = 3001;
+
+    app.post('/API/send-email', function (req, res) {
+
       let transporter = nodeMailer.createTransport({
           service: 'gmail',
           host: 'smtp.gmail.com',
           port: 465,
           secure: true,
           auth: {
-              user: emailHost,
-              pass: 'Cambiar1@'
-          }
+              user: emailUser,
+              pass: emailPassword
+          } 
       });
+
       let mailOptions = {
-          from: `"API Messages" ${emailHost}`, // sender address
+          from: `${emailUser}`, // sender address
           to: 'joaquinvillanuevafarber@gmail.com', // list of receivers
-          subject: req.body.subject, // Subject line
-          text: req.body.body, // plain text body
-          html: '<b>Saga Email</b>' // html body
+          subject: `Sending Email using Node.js - ${req.body.email}`, // Subject line
+          html: `<h2>Nuevo mensaje de: ${req.body.name}</h2>
+          </br>
+          <h3>${req.body.email}</h3>
+          </br>
+          <b>${req.body.subject}</b>
+          </br>
+          <p>${req.body.messageBody}</p>` // html body
       };
 
-      transporter.sendMail(mailOptions, (error, info) => {
+      transporter.sendMail(mailOptions, (error, info, res) => {
           if (error) {
               return console.log(error);
           }
           console.log('Message %s sent: %s', info.messageId, info.response);
-              res.render('index');
-          });
+          console.log('The text is: ' + req.body.messageBody);
+
+          if (info.response.includes("OK")){
+              console.log("Mensaje enviado con exito.");
+              var statusCode = 200;
+              return res.body = statusCode;
+          }
+        });
       });
           app.listen(port, function(){
             console.log('Server is running at port: ',port);
