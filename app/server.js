27d8a7447327ffc733b6735c833009dda06229eb
Joaquin Villanueva <joaquinvillanuevafarber@gmail.com>
2022-07-14 16:05:54 -0300

    first commit

--- app/server.js
+++ app/server.js
@@ -1,12 +1,59 @@
-var express = require('express')
-var app = express()               
+var express = require('express'),
+    path = require('path'),
+    nodeMailer = require('nodemailer'),
+    bodyParser = require('body-parser');
 
-var port = process.env.PORT || 8080
+    var emailUser = "saga.mensajes@gmail.com";
+    var emailPassword = "uliqlcqkoaibeqrh";
 
-app.post('/mensaje', function(req, res) {
-  res.json({ mensaje: 'Método post' })   
-})
+    var app = express();
+    app.use(express.static('public'));
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    var port = 3001;
 
-// iniciamos nuestro servidor
-app.listen(port)
-console.log('API escuchando en el puerto ' + port)
+    app.post('/API/send-email', function (req, res) {
+        let statusCode = 200;
+
+      let transporter = nodeMailer.createTransport({
+          service: 'gmail',
+          host: 'smtp.gmail.com',
+          port: 465,
+          secure: true,
+          auth: {
+              user: emailUser,
+              pass: emailPassword
+          } 
+      });
+
+      let mailOptions = {
+          from: `${emailUser}`, // sender address
+          to: 'joaquinvillanuevafarber@gmail.com', // list of receivers
+          subject: `New email from - ${req.body.email}`, // Subject line
+          html: `<h2>Nuevo mensaje de: ${req.body.name}</h2>
+          </br>
+          <h3>${req.body.email}</h3>
+          </br>
+          <b>${req.body.subject}</b>
+          </br>
+          <p>${req.body.messageBody}</p>` // html body
+      };
+
+      transporter.sendMail(mailOptions, (error, info, res) => {
+          if (error) {
+                statusCode = 404;
+              return console.log(error);
+          }
+          console.log('Message %s sent: %s', info.messageId, info.response);
+
+          if (info.response.includes("OK")){
+              statusCode = 200      
+              var messageDone = "Mensaje enviado con exito.";       
+              return console.log(messageDone);
+          }
+        });
+        return res.sendStatus(statusCode);
+      });
+          app.listen(port, function(){
+            console.log('Server is running at port: ',port);
+          });
