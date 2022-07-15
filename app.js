c58eb5587c0033b58e7c966f43d307f661fce9bb
Joaquin <joaquinvillanuevafarber@gmail.com>
2022-07-15 14:00:26 -0300

    home commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require('express'),
     app.use(bodyParser.json());
     var port = process.env.PORT || 3001;
 
-    app.post('/API/send-email', function (req, res) {
+    app.post('/', function (req, res) {
         let statusCode = 200;
 
       let transporter = nodeMailer.createTransport({
