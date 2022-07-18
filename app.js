86fc8ce139d97a9036e71e303dc241f6fb506937
Joaquin <joaquinvillanuevafarber@gmail.com>
2022-07-18 16:46:22 -0300

    cors added

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ var express = require('express'),
     nodeMailer = require('nodemailer'),
     bodyParser = require('body-parser');
 
-    var emailUser = "saga.mensajes@gmail.com";
+    var emailUser = "joaquinvillanuevafarber@gmail.com";
     var emailPassword = "uliqlcqkoaibeqrh";
 
     var app = express();
+    
+    const cors=require("cors");
+    const corsOptions ={
+       origin:'*', 
+       credentials:true,            //access-control-allow-credentials:true
+       optionSuccessStatus:200,
+    }
+    
+    app.use(cors(corsOptions)) // Use this after the variable declaration
     app.use(express.static('public'));
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
